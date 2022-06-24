d3823fc357776eef06ec130c995e1d673d395140
Gota7 <gotaseven@gmail.com>
2022-06-23 23:51:55 -0400

    README Fixes

--- Tests/HelloWorld/Program.cs
+++ Tests/HelloWorld/Program.cs
@@ -12,18 +12,22 @@ ProgramBuilder pb = new ProgramBuilder();
 UnitBuilder ub = pb.NewCompilationUnit("HelloWorld");
 
 // External function for printing statements.
+// This is part of the C library so we must tell the compiler to not mangle the name.
 // Notice that it has no definition.
-// The input parameter is a constant so we only give it the read permission.
+// The input parameter is a constant, so we only give it the read permission.
 ub.AddFunction(new Function(
     "puts",
-    new VarTypeFunction(VarType.Int, null, new Variable("str", VarType.String, DataAccessFlags.Read))
+    new VarTypeFunction(VarType.Int, null, new Variable("str", VarType.String, DataAccessFlags.Read)),
+    new ItemAttribute("NoMangle")
 ));
 
 // Build the main function by first defining it, then formally adding it as a function.
+// The main function is special and will not be mangled.
 // Notice that we don't have to add the code to the function immediately like we do here, we could have prepared it in advance.
 // A function is a variable, so we can just fetch it with a variable expression and WARD will automatically resolve it.
 CodeBuilder cb = new CodeBuilder();
-cb.Code(new ExpressionCall(new ExpressionVariable("puts"), new ExpressionConstString("Hello World!")));
+// This line below should be uncommented, but call and variable expressions aren't done yet :<
+//cb.Code(new ExpressionCall(new ExpressionVariable("puts"), new ExpressionConstString("Hello World!")));
 cb.Code(new StatementReturn(new ExpressionConstInt(VarType.Int, 0)));
 ub.AddFunction(new Function(
     "main",
